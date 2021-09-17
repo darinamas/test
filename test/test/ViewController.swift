f3e6d320ac4cc29faaf39f512f7d738e54adaa98
Darina <daryna.massive@gmail.com>
2021-09-17 20:05:04 +0200

    1

---
+++ test/test/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  test
+//
+//  Created by Daryna Polevyk on 17.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    func func1() {
+        print("func1")
+    }
+}
+
